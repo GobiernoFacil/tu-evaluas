e4095d759923b792817246701fdd992e040f7a52
el coruco <el.coruco@gmail.com>
2016-06-23 17:42:37 -0500

    modify the session times

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
     |
     */
 
-    'lifetime' => 1200,
+    'lifetime' => 180,
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
