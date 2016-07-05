e14cea196fef75da580e4583dfa7c5c4ee3a773f
elcoruco <el.coruco@gmail.com>
2016-07-05 16:24:47 -0500

    mini hack for conacyt

--- resources/views/email/invitation.blade.php
+++ resources/views/email/invitation.blade.php
@@ -13,11 +13,16 @@
    			height: 6px;
 			border-left-width: 35px">
        	<p style="font-family: 'Open Sans Light', Helvetica, Arial, sans-serif ;color:#545454; font-size:16px; line-height:1.428; text-align:justify">
-        Bienvenido a la plataforma Tú Evalúas mediante la cual podrás expresar tu opinión acerca de los programas del Gobierno Federal. 
-    	Para ello, te pedimos que nos ayudes a responder una encuesta que te tomará alrededor de 5 minutos.</p>
+        <!--Bienvenido a la plataforma Tú Evalúas mediante la cual podrás expresar tu opinión acerca de los programas del Gobierno Federal. 
+    	Para ello, te pedimos que nos ayudes a responder una encuesta que te tomará alrededor de 5 minutos.-->
+       Bienvenido a la plataforma Tú Evalúas mediante la cual podrás expresar tu opinión acerca de los Fondos Sectoriales del CONACYT. Para ello, te pedimos que nos ayudes a responder una encuesta que te tomará alrededor de 5 minutos. 
+      </p>
 
 		<p style="font-family: 'Open Sans Light', Helvetica, Arial, sans-serif ;color:#545454; font-size:16px; line-height:1.428; text-align:justify">
         Toda la información que nos proporciones será confidencial y utilizada únicamente para conocer tu opinión de los apoyos o servicios que recibes.</p>
+
+        <p style="font-family: 'Open Sans Light', Helvetica, Arial, sans-serif ;color:#545454; font-size:16px; line-height:1.428; text-align:justify">
+        Por favor haz caso omiso a este correo si ya respondiste esta encuesta.</p>
 
 		<p style="font-family: 'Open Sans Light', Helvetica, Arial, sans-serif ;color:#545454; font-size:16px; line-height:1.428; text-align:justify">
         Si estás de acuerdo con participar en la encuesta, por favor da clic en el siguiente vínculo: <a href="{{url('encuesta/' . $applicant->form_key)}}" style="color:#12C">tuevaluas.com.mx</a></p>
