7c7a636a0400fe6be0b43d180aade8624c8a4f94
Boris Cuapio Morales <el.coruco@gmail.com>
2016-12-04 19:27:25 -0600

    docker 35

--- public/js/views/section_view.js
+++ public/js/views/section_view.js
@@ -72,7 +72,7 @@ define(function(require){
       this.collection.each(function(question){
       // [1] genera una colección de opciones para cada pregunta. La lista de opciones
       //     está disponible en el controller.
-        var opt = new Backbone.Collection(this.controller.q_options.where({question_id : question.id}));
+        var opt = new Backbone.Collection(this.controller.q_options.where({question_id : Number(question.id)}));
       // [2] se genera un View con la pregunta, y se le pasan las siguiente variables:
       //     * el modelo de la pregunta (question)
       //     * la colección de opciones (opt)
